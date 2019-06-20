63dd8777bbc348e94d88e4ef7eac0886a7ad41e5
Manolito Mason Pimentel <mason@mpimentel.net>
2019-06-19 22:35:07 -0700

    6: directly pass the message to timedate

--- src/timedate.py
+++ src/timedate.py
@@ -2,14 +2,7 @@ from lib.messages import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("val")
+parser.add_argument("msg")
 args = parser.parse_args()
 
-if args.val == "1":
-    print_message("send.py not running", "CRONJOB", "verbose")
-elif args.val == "2":
-    print_message("receive.py not running", "CRONJOB", "verbose")
-elif args.val == "3":
-    print_message("Re-running send and receive", "CRONJOB", "verbose")
-elif args.val == "4":
-    print_message("Still running", "CRONJOB", "verbose")
+print_message(args.msg, "CRONJOB", "verbose")
